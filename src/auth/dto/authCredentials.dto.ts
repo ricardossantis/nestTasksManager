299179d6fe9c49638c5dc3c7e106c2d68e9b9428
Ricardo <ricardo.santis@ewally.com.br>
2022-08-25 17:19:24 -0300

    feat: create user with hashed password

--- src/auth/dto/authCredentials.dto.ts
+++ src/auth/dto/authCredentials.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Passwords will contain at least 1 upper case letter / Passwords will contain at least 1 lower case letter / Passwords will contain at least 1 number or special character',
+  })
   password: string;
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,18 +1,35 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { constants } from '../constants/userConstants';
+import * as bcrypt from 'bcrypt';
 import { AuthCredentialsDto } from './dto/authCredentials.dto';
+import {
+  InternalServerErrorException,
+  ConflictException,
+} from '@nestjs/common';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<string> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = await this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === constants.duplicateUser) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
 
     return `User created`;
   }
